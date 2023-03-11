f9a7ae8c77c80aa20164333cd47726f8922c59e7
carla <carla.zollaa@gmail.com>
2023-03-11 20:55:37 -0300

    fix: form create and edit workout

--- src/components/Form/workoutCreateForm/index.tsx
+++ src/components/Form/workoutCreateForm/index.tsx
@@ -1,21 +1,27 @@
-import { Training } from "./training";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { AdminContext, IResponseWorkout } from "../../../context/AdminContext";
 import { StyledForm } from "./style";
 import { useContext } from "react";
 import Input from "../Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schemaRegisterNewWorkout } from "../../../utils/schemas";
+
+
+export type IRegisterWorkout = Omit<IResponseWorkout, "id">
 
 export const WorkoutForm = () => {
-  const submitWorkoutForm: SubmitHandler<IResponseWorkout> = (
+ 
+  const { createWorkout } = useContext(AdminContext);
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<IRegisterWorkout>({
+    resolver: yupResolver(schemaRegisterNewWorkout)
+  });
+  const submitWorkoutForm: SubmitHandler<IRegisterWorkout> = (
     dataWorkout
   ) => {
     createWorkout(dataWorkout);
-    console.log(dataWorkout.workout)
     reset();
   };
-  const { createWorkout } = useContext(AdminContext);
-  const { register, handleSubmit, reset, control } =
-    useForm<IResponseWorkout>();
 
   return (
     <>
@@ -24,18 +30,40 @@ export const WorkoutForm = () => {
           label="Tipo de treino"
           type="text"
           register={register("workout_type")}
+          error={errors.workout_type}
         />
         <Input
           label="Grupo muscular"
           type="text"
           register={register("muscle_group")}
+          error={errors.muscle_group}
+
         />
         <Input
-          label="Tempo de treino"
+          label="Nome do treino"
           type="text"
-          register={register("workout_time")}
+          register={register("workout")}
+          error={errors.workout}
         />
-        <Training register={register} control={control} />
+         <Input
+          label="Repetições"
+          type="text"
+          register={register("repetitions")}
+          error={errors.repetitions}
+        />
+        <Input
+          label="Séries"
+          type="text"
+          register={register("series")}
+          error={errors.series}
+        />
+        <Input
+          label="Carga"
+          type="text"
+          register={register("charge")}
+          error={errors.charge}
+        />
+
         <button type="submit">Cadastrar treino</button>
       </StyledForm>
     </>
--- src/components/workoutList/workoutCard/index.tsx
+++ src/components/workoutList/workoutCard/index.tsx
@@ -9,7 +9,10 @@ export const WorkoutCard = () => {
       {workoutList.map((workout, index) => (
         <WorkoutItem key={index}>
           <h3>{workout.workout_type}</h3>
-          <span>{workout.muscle_group}</span>
+            <div className="contentWorkout">
+              <span>{workout.muscle_group} </span>
+              <span className="quantities">{workout.series} X {workout.repetitions} - {workout.charge}</span>
+            </div>
           <button onClick={()=> {setModalEdit(true), setTraining(workout)}}>+</button>
         </WorkoutItem>
       ))}
