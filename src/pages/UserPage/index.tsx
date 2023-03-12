5ca203fb20df3e4653a0a4299c28ade28c6ba681
carla <carla.zollaa@gmail.com>
2023-03-12 11:29:16 -0300

    fix: comment in userPage

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -15,6 +15,8 @@ const UserPage = () => {
 
     const [ openModal, setOpenModal] = useState(false)
     const { training, trainings} = useContext(AdminContext)
+
+    //ta errado puxar do contexto do admin o estado que armazena os treinos, existe uma rota na api que faz esse get e o user comum é autorizado
     
     console.log(training)
     console.log(trainings)
